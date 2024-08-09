13cfeff582914ecb467e191aa93a1675b3e48d13
AndriellyKu <137730881+AndriellyKu@users.noreply.github.com>
2024-08-09 08:35:49 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -1,7 +1,9 @@
-document.getElementById('pencil-icon').addEventListener('click', function() {
-    document.getElementById('Container_drop').innerHTML = `
-        <div class="container_textarea">
-            <textarea id="text-input" placeholder="Escreva aqui"></textarea>
-        </div>
-    `;
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('pencil_icon').addEventListener('click', function() {
+        document.getElementById('Container_drop').innerHTML = `
+            <div class="container_textarea">
+                <textarea id="text-input" placeholder="Escreva aqui"></textarea>
+            </div>
+        `;
+    });
 });
