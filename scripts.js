55f7cb089c1a70fbfaea53be185495e284cff795
AndriellyKu <137730881+AndriellyKu@users.noreply.github.com>
2024-08-09 08:53:49 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('pencil_icon').addEventListener('click', function() {
+        document.getElementById('Container_drop').innerHTML = `
+            <div class="container_textarea">
+                <textarea id="text-input" placeholder="Escreva aqui o conteudo o qual deseja gerar o teste"></textarea>
+            </div>
+        `;
+    });
+});
